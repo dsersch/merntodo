90f89ceeda723c2f58f1b949e857048260305eac
David Sersch <dsersch640@gmail.com>
2021-09-20 14:00:01 -0400

    server and db setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT;
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Database connection successful...")
+});
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.listen(PORT, (err) => {
+    err || console.log(`Server running on ${PORT}...`)
+})
