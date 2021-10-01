28b01d11e39f724670960ea16b3d510f70bc76e3
dave <dsersch640@gmail.com>
2021-10-01 09:26:03 -0400

    finished status added

--- client/src/components/layout/ToDoItem.js
+++ client/src/components/layout/ToDoItem.js
@@ -2,6 +2,29 @@ import React from 'react';
 import classes from './ToDoItem.module.css';
 
 const ToDoItem = (props) => {
+    const onFinished = async () => {
+        try {
+            const res = await fetch(`/items/${props.data._id}`, {
+                method: 'PATCH',
+                body: JSON.stringify({
+                    finished: !props.data.finished
+                }),
+                headers: {
+                    "Content-type": "application/json",
+                    "Authorization": `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+
+            const result = await res.json()
+            if (result.status === 'success') {
+                props.getItems()
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+
     const onDelete = async () => {
         try {
             const res = await fetch(`/items/${props.data._id}`, {
@@ -17,14 +40,21 @@ const ToDoItem = (props) => {
         }
     }
 
+    let classList = classes['to-do-item']
+
+    if (props.data.finished) {
+        classList = `${classes['to-do-item']} ${classes['finished-item']}`
+    }
+
     return (
-        <div className={classes['to-do-item']}>
+        <div className={classList}>
             <div>
                 <h1>{props.data.title}</h1>
                 <p>{props.data.description}</p>
             </div>
-            <div>
-                <button onClick={onDelete}>X</button>
+            <div className={classes.controls}>
+                <button className={classes.finished} onClick={onFinished}>✔</button>
+                <button className={classes.delete} onClick={onDelete}>X</button>
             </div>
         </div>
     )
