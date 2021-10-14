8d133f98ff19e6206b4b67dd78cd72149e077665
dave <dsersch640@gmail.com>
2021-10-14 10:38:02 -0400

    deployment prep

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
@@ -27,6 +28,14 @@ app.use(express.json());
 
 app.use('/items', listItemRouter);
 app.use('/auth', authRouter);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(PORT, (err) => {
     err || console.log(`Server running on ${PORT}...`)
